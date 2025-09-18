574b67d3aa512026f9643db2f0bc005ae2186825
Ethen1264 <jxorhks86909763@gmail.com>
2025-09-18 12:13:28 +0900

    feat: 메모리 라우팅 동작 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/home";
+import Test from "./page/test";
+
 function App() {
-  return <h1>하이</h1>;
+  return (
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </MemoryRouter>
+  );
 }
 
 export default App;
---
+++ src/page/test/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = () => {
+  return <h1>테스트페이지</h1>;
+};
+
+export default Test;
